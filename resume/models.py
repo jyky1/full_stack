4d8cd0506627b549620b2fb95745ba3e4e63afab
jyky <jykymyrza@gmail.com>
2023-02-13 14:42:12 +0600

    add install apps

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+
+class Resume(models.Model):
+    name = models.ForeignKey(User, on_delete=models.CASCADE, related_name='name')
+    email = models.ForeignKey(User, on_delete=models.CASCADE, related_name='email')
+    phone = models.ForeignKey(User, on_delete=models.CASCADE, related_name='phone')
+    city = models.ForeignKey(User, on_delete=models.CASCADE, related_name='country')
+    git_hub = models.URLField(blank=True)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.phone
+
+    class Meta:
+        verbeuse_name = 'Resume'
+        plural_verbouse_name = 'Resumes'
+
+
