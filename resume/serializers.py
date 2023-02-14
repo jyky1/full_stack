f69571f0c1e92d2fb787b434a061b6b1d53204e8
jyky <jykymyrza@gmail.com>
2023-02-14 16:49:00 +0600

    add game

--- resume/serializers.py
+++ resume/serializers.py
@@ -3,17 +3,17 @@ from rest_framework import serializers
 from .models import Resume
 
 class ResumeSerializer(serializers.ModelSerializer):
-    name = serializers.ReadOnlyField()
-    email = serializers.ReadOnlyField()
-    phone = serializers.ReadOnlyField()
-    city = serializers.ReadOnlyField()
+    # name = serializers.ReadOnlyField()
+    # email = serializers.ReadOnlyField()
+    # phone = serializers.ReadOnlyField()
+    # city = serializers.ReadOnlyField()
 
     class Meta:
         model = Resume
         fields = '__all__'
 
-    def create(self, validated_data):
-        requests = self.context.get('request')
-        user = requests.user
-        resume = Resume.objects.create(name=user, **validated_data)
-        return resume
+    # def create(self, validated_data):
+    #     requests = self.context.get('request')
+    #     user = requests.user
+    #     resume = Resume.objects.create(name=user, **validated_data)
+    #     return resume
