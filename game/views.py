c07e1437273d9a5c1eaa89c2bb35e0544352093e
jyky <jykymyrza@gmail.com>
2023-02-15 00:30:20 +0600

    add permissions for resume and game and rating

--- game/views.py
+++ game/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser, AllowAny
 
 from .serializers import GameSerializer, RatingSerializer
 from .models import Game, Rating
+from .permissions import IsAuthor
+
+
+class IsAdminMixin:
+    def get_permissions(self):
+        if self.action == 'create':
+            permissions = [IsAdminUser, ]
+        elif self.action in ['update', 'partical_update', 'destroi']:
+            permissions = [IsAdminUser,]
+        else:
+            permissions=[AllowAny,]
+        return [permission() for permission in permissions]
 
 class GameView(ModelViewSet):
     serializer_class = GameSerializer
     queryset = Game.objects.all()
 
+class UserPermissionMixin:
+    def get_permissions(self):
+        if self.action == 'create':
+            permissions = [IsAuthor, ]
+        elif self.action in ['update', 'partical_update', 'destroi']:
+            permissions = [IsAuthor,]
+        else:
+            permissions=[AllowAny,]
+        return [permission() for permission in permissions]
 
-class RatingView(ModelViewSet):
+class RatingView(UserPermissionMixin ,ModelViewSet):
     serializer_class = RatingSerializer
     queryset = Rating.objects.all()
--- resume/views.py
+++ resume/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
 
 from .serializers import ResumeSerializer
 from .models import Resume
+from .permissions import IsAuthor
 
+class UserPermissionMixin:
+    def get_permissions(self):
+        if self.action == 'create':
+            permissions = [IsAuthor, ]
+        elif self.action in ['update', 'partical_update', 'destroi']:
+            permissions = [IsAuthor,]
+        else:
+            permissions=[AllowAny,]
+        return [permission() for permission in permissions]
 
-class ResumeView(ModelViewSet):
+class ResumeView(UserPermissionMixin, ModelViewSet):
     queryset=Resume.objects.all()
     serializer_class=ResumeSerializer
