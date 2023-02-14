39287c191febe9ba08020291ccc02b8a57bda199
Iman <lmankadyrov29@gmail.com>
2023-02-14 13:49:19 +0600

    serializer

--- account/serializers.py
+++ account/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, UserManager
+from .tasks import send_activation_code_celery
 
-class UserSerializer(serializers.ModelSerializer):
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=4, required=True, write_only=True)
+    password_confirm = serializers.CharField(min_length=4, required=True, write_only=True)
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('email', 'password', 'password_confirm')
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password_confirm = attrs.pop('password_confirm')
+        if password != password_confirm:
+            raise serializers.ValidationError('password do not match')
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        send_activation_code_celery.delay(user.email, user.activation_code)
+        return user
--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,23 @@
 from django.urls import path
-from .views import OrderView
+from .views import RegisterView, ActivationView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('activate/<str:email>/<str:activation_code>/', ActivationView.as_view(), name='activate'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
+
+# from django.urls import path, include
+# from .views import RegisterView, ActivationView
+# from rest_framework.routers import DefaultRouter
+
+# router = DefaultRouter()
+# router.register('rating', RegisterView)
+# router.register('comments', ActivationView)
+
+# urlpatterns = [
+#     path('',  include(router.urls)),
+# ]
